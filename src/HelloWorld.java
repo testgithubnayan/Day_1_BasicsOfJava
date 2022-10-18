2a5d09458cc97c0af3a21761b9e4866561d2e4cd
NAYAN SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-10-18 17:04:10 +0530

    'hello java

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.brigelabz;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+    	System.out.println("Hello Java");
+    
+    int c1, c2, c3,sum = 0;
+
+		c1 = Integer.parseInt(args[0]);
+		c2 = Integer.parseInt(args[1]);
+		c3 = Integer.parseInt(args[1]);
+		 sum = c1 + c2 + c3;
+		
+	}
+}
